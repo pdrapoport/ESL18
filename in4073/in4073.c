037d6ce538be3dadd8dde9f95beb97a15c677468
Unknown <ruearjj232a224@gmail.com>
2018-05-08 14:02:20 +0200

    FSM for the FCB implemented in this file
    It grabs a key imput from the PC-terminal and changes the mode of the drone.
    Mode functions to be defined
    Implementation of protocol is needed in order to send and read the commands (right now no protocol is being used).

--- in4073/in4073.c
+++ in4073/in4073.c
@@ -15,6 +15,217 @@
 
 #include "in4073.h"
 
+
+enum states {
+  Safe_Mode,            // Mode 0
+  Panic_Mode,           // Mode 1
+  Manual_Mode,          // Mode 2
+  Calibration_Mode,     // Mode 3
+  Yaw_Mode,             // Mode 4
+  Full_Mode,            // Mode 5
+  Raw_Mode,             // Mode 6
+  Height_Mode,          // Mode 7
+  Wireless_Mode         // Mode 8
+} state;
+
+bool motors_off = true; // Update according to the readings
+bool no_failure = true; // Update
+bool calibration_done = false; // Update after the calibration is done
+enum states state;
+
+/*------------------------------------------------------------------
+ * FSM FCB
+ *------------------------------------------------------------------
+ */
+
+const char* getCurrentState(enum states state)
+{
+   switch (state)
+   {
+      case Safe_Mode: return "Safe_Mode";
+      case Panic_Mode: return "Panic_Mode";
+      case Manual_Mode: return "Manual_Mode";
+      case Calibration_Mode: return "Calibration_Mode";
+      case Yaw_Mode: return "Yaw_Mode";
+      case Full_Mode: return "Full_Mode";
+      case Raw_Mode: return "Raw_Mode";
+      case Height_Mode: return "Height_Mode";
+      case Wireless_Mode: return "Wireless_Mode";
+      default: return "Error";
+   }
+}
+
+void step(enum states *state, int c) {
+  switch (*state) {
+
+    // SAFE MODE
+    case Safe_Mode:
+        switch (c){
+          case '2':
+            if (motors_off && no_failure){
+              *state = Manual_Mode;
+              printf("Manual_Mode Selected\n");
+              // Call for Manual_Mode function required
+            }
+            break;
+
+          case '3':
+            if (motors_off && no_failure){
+              *state = Calibration_Mode;
+              printf("Calibration_Mode Selected\n");
+              // Call for Calibration_Mode function required
+              //delay(2000);
+              printf("Calibration performed\n");
+              calibration_done = true; //Change after the calibration is done
+              if (motors_off && calibration_done){
+                *state = Safe_Mode;
+                printf("Safe_Mode Selected\n");
+              }
+            }
+            break;
+
+          case '4':
+            if (motors_off && no_failure && calibration_done){
+              *state = Yaw_Mode;
+              printf("Yaw_Mode Selected\n");
+              // Call for Yaw_Mode function required
+            }
+            break;
+
+          case '5':
+            if (motors_off && no_failure && calibration_done){
+              *state = Full_Mode;
+              printf("Full_Mode Selected\n");
+              // Call for Full_Mode function required
+            }
+            break;
+
+          case '6':
+            if (motors_off && no_failure && calibration_done){
+              *state = Raw_Mode;
+              printf("Raw_Mode Selected\n");
+              // Call for Raw_Mode function required
+            }
+            break;
+
+          case '7':
+            if (motors_off && no_failure && calibration_done){
+              *state = Height_Mode;
+              printf("Height_Mode Selected\n");
+              // Call for Height_Mode function required
+            }
+            break;
+
+          case '8':
+            if (motors_off && no_failure && calibration_done){
+              *state = Wireless_Mode;
+              printf("Wireless_Mode Selected\n");
+              // Call for Full_Mode function required
+            }
+            break;
+
+          default:
+            printf("No mode selected\n");
+            break;
+      }
+      break;
+
+      // MANUAL MODE
+      case Manual_Mode:
+        if (!no_failure)
+          *state = Panic_Mode;
+          // Call for Panic_Mode function required
+        else if (c == '0' && motors_off){
+          *state = Safe_Mode;
+          printf("Safe_Mode Selected\n");
+          // Call for Safe_Mode function required
+        }
+        else{
+          printf("No mode selected\n");
+        }
+        break;
+
+      // YAW MODE
+      case Yaw_Mode:
+        if (!no_failure)
+          *state = Panic_Mode;
+          // Call for Panic_Mode function required
+        else if (c == '0' && motors_off){
+          *state = Safe_Mode;
+          printf("Safe_Mode Selected\n");
+          // Call for Safe_Mode function required
+        }
+        else{
+          printf("No mode selected\n");
+        }
+        break;
+
+      // FULL MODE
+      case Full_Mode:
+        if (!no_failure)
+          *state = Panic_Mode;
+          // Call for Panic_Mode function required
+        else if (c == '0' && motors_off){
+          *state = Safe_Mode;
+          printf("Safe_Mode Selected\n");
+          // Call for Safe_Mode function required
+        }
+        else{
+          printf("No mode selected\n");
+        }
+        break;
+
+      // RAW MODE
+      case Raw_Mode:
+        if (!no_failure)
+          *state = Panic_Mode;
+          // Call for Panic_Mode function required
+        else if (c == '0' && motors_off){
+          *state = Safe_Mode;
+          printf("Safe_Mode Selected\n");
+          // Call for Safe_Mode function required
+        }
+        else{
+          printf("No mode selected\n");
+        }
+        break;
+
+      // HEIGHT MODE
+      case Height_Mode:
+        if (!no_failure)
+          *state = Panic_Mode;
+          // Call for Panic_Mode function required
+        else if (c == '0' && motors_off){
+          *state = Safe_Mode;
+          printf("Safe_Mode Selected\n");
+          // Call for Safe_Mode function required
+        }
+        else{
+          printf("No mode selected\n");
+        }
+        break;
+
+      // WIRELESS MODE
+      case Wireless_Mode:
+        if (!no_failure)
+          *state = Panic_Mode;
+          // Call for Panic_Mode function required
+        else if (c == '0' && motors_off){
+          *state = Safe_Mode;
+          printf("Safe_Mode Selected\n");
+          // Call for Safe_Mode function required
+        }
+        else{
+          printf("No mode selected\n");
+        }
+        break;
+			case Panic_Mode:
+				break;
+			case Calibration_Mode:
+				break;
+    }
+}
+
 /*------------------------------------------------------------------
  * process_key -- process command keys
  *------------------------------------------------------------------
@@ -55,9 +266,36 @@ void process_key(uint8_t c)
 		case 27:
 			demo_done = true;
 			break;
-		case '0':
+		case '1':
 			nrf_gpio_pin_toggle(RED);
 			break;
+  	case '2':
+    	step(&state,'2');
+    	break;
+    case '3':
+   	  step(&state,'3');
+    	break;
+    case '4':
+      step(&state,'4');
+      break;
+    case '5':
+      step(&state,'5');
+    	break;
+    case '6':
+    	step(&state,'6');
+    	break;
+    case '7':
+      step(&state,'7');
+    	break;
+    case '8':
+      step(&state,'8');
+      break;
+    case '0':
+      step(&state,'0');
+      break;
+    case 'p':
+      printf("%s\n",getCurrentState(state));
+      break;
 		default:
 			nrf_gpio_pin_toggle(RED);
 	}
@@ -74,19 +312,19 @@ int main(void)
 	timers_init();
 	adc_init();
 	twi_init();
-	imu_init(true, 100);	
+	imu_init(true, 100);
 	baro_init();
 	spi_flash_init();
 	ble_init();
 
 	uint32_t counter = 0;
 	demo_done = false;
-
+	state = Safe_Mode;
 	while (!demo_done)
 	{
 		if (rx_queue.count) process_key( dequeue(&rx_queue) );
 
-		if (check_timer_flag()) 
+		if (check_timer_flag())
 		{
 			if (counter++%20 == 0) nrf_gpio_pin_toggle(BLUE);
 
@@ -102,12 +340,12 @@ int main(void)
 			clear_timer_flag();
 		}
 
-		if (check_sensor_int_flag()) 
+		if (check_sensor_int_flag())
 		{
 			get_dmp_data();
 			run_filters_and_control();
 		}
-	}	
+	}
 
 	printf("\n\t Goodbye \n\n");
 	nrf_delay_ms(100);
